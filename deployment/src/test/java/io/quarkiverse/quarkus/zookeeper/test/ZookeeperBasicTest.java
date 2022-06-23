f2ed8daa3824fefd5e3b095c93476676ea96765e
Riccardo Pasquini <r.pasquini@almawave.it>
2022-06-23 21:39:22 +0200

    Test basic configuration
    Signed-off-by: Riccardo Pasquini <r.pasquini@almawave.it>

---
+++ deployment/src/test/java/io/quarkiverse/quarkus/zookeeper/test/ZookeeperBasicTest.java
@@ -0,0 +1,121 @@
+package io.quarkiverse.quarkus.zookeeper.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+import org.apache.zookeeper.ZooKeeper;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
+import org.testcontainers.containers.GenericContainer;
+
+import io.quarkus.test.QuarkusUnitTest;
+
+public class ZookeeperBasicTest {
+
+    private static final Logger LOG = Logger.getLogger(ZookeeperBasicTest.class);
+
+    // Start unit test with your extension loaded
+    @RegisterExtension
+    static final QuarkusUnitTest unitTest = new QuarkusUnitTest()
+            .withConfigurationResource("basic-connection.properties")
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
+
+    @SuppressWarnings("deprecation")
+    // Using a fixed hostPort to match the config property as defined in the basic-connection.properties
+    private static final GenericContainer<?> ZOOKEEPER = new FixedHostPortGenericContainer<>("zookeeper:3.8.0")
+            .withFixedExposedPort(32181, 2181);
+
+    @BeforeAll
+    public static void startZookeeper() {
+        ZOOKEEPER.start();
+    }
+
+    @AfterAll
+    public static void stopZookeeper() {
+        ZOOKEEPER.stop();
+    }
+
+    @ConfigProperty(name = "quarkus.zookeeper.session.connectionString")
+    String cnString;
+
+    @ConfigProperty(name = "quarkus.zookeeper.session.timeout")
+    int cnTimeout;
+
+    @ConfigProperty(name = "quarkus.zookeeper.session.canBeReadOnly")
+    boolean canBeReadOnly;
+
+    @Test
+    public void testBasicConnection() throws IOException {
+
+        assertEquals("127.0.0.1:32181", cnString);
+        assertEquals(15, cnTimeout);
+        assertTrue(canBeReadOnly);
+
+        LOG.infof("Zookeeper connection string is [%s]", cnString);
+
+        final Phaser testCoordinator = new Phaser(2);
+        final AtomicBoolean connected = new AtomicBoolean(false);
+        var zk = new ZooKeeper(cnString, cnTimeout,
+                new Watcher() {
+
+                    @Override
+                    public void process(WatchedEvent event) {
+                        LOG.infof("Processing event for state [%s] and type [%s]", event.getState(), event.getType());
+                        if (KeeperState.SyncConnected == event.getState()) {
+                            LOG.info("Client connected to zookeeper");
+                            connected.compareAndSet(false, true);
+                            testCoordinator.arrive();
+                        } else if (KeeperState.Closed == event.getState()) {
+                            LOG.info("Client connection closed");
+                            testCoordinator.arriveAndDeregister();
+                        }
+                    }
+                }, canBeReadOnly);
+
+        assertFalse(connected.get());
+        var testPhase = testCoordinator.arrive();
+        try {
+            testCoordinator.awaitAdvanceInterruptibly(testPhase, 30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+        assertTrue(connected.get());
+
+        try {
+            zk.close(30000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        } finally {
+            var finalPhase = testCoordinator.arriveAndDeregister();
+            try {
+                testCoordinator.awaitAdvanceInterruptibly(finalPhase, 30, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            } catch (TimeoutException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
--- integration-tests/src/test/java/io/quarkiverse/quarkus/zookeeper/it/ZookeeperTestServer.java
+++ integration-tests/src/test/java/io/quarkiverse/quarkus/zookeeper/it/ZookeeperTestServer.java
@@ -2,17 +2,27 @@ package io.quarkiverse.quarkus.zookeeper.it;
 
 import java.util.Map;
 
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 
 public class ZookeeperTestServer implements QuarkusTestResourceLifecycleManager {
 
+    private static final GenericContainer<?> ZOOKEEPER = new GenericContainer<>(DockerImageName.parse("zookeeper:3.8.0"))
+            .withAccessToHost(true)
+            .withExposedPorts(2181);
+
     @Override
     public Map<String, String> start() {
+        ZOOKEEPER.start();
         return Map.of(
-                "quarkus.zookeeper.session.connectionString", "localhost:2181");
+                "quarkus.zookeeper.session.connectionString",
+                String.format("%s:%d", ZOOKEEPER.getHost(), ZOOKEEPER.getMappedPort(2181)));
     }
 
     @Override
     public void stop() {
+        ZOOKEEPER.stop();
     }
 }
